e4888fb677d071e85d04e7523a42b6cd74e384ae
Jun al-Fajr <jun.fajr@gmail.com>
2022-11-24 10:45:53 +0700

    install router-dom

--- src/App.js
+++ src/App.js
@@ -1,36 +1,14 @@
-import { useEffect } from 'react'
-import { useDispatch, useSelector } from 'react-redux'
-import { fetchPost } from './redux/slices/counterSlices'
-import './App.css'
-import logo from './logo.svg'
+import { BrowserRouter, Routes, Route } from 'react-router-dom'
+import HomePage from './components/HomePage/HomePage'
 
 function App() {
-    const dispatch = useDispatch()
-    useEffect(() => {
-        dispatch(fetchPost())
-    }, [])
-
-    //select store state
-    const post = useSelector(state => state.post)
-    const { postsList, loading } = post
-    console.log({ postsList, loading })
-    return (
-        <div className='App'>
-            <h1>Post List</h1>
-            <hr />
-            {loading ? (
-                <h2>Loading...</h2>
-            ) : (
-                postsList?.map(post => (
-                    <>
-                        <h2>{post?.title}</h2>
-                        <p>{post?.body}</p>
-                        <hr />
-                    </>
-                ))
-            )}
-        </div>
-    )
+  return (
+    <BrowserRouter>
+      <Routes>
+        <Route exact path='/' element={<HomePage />} />
+      </Routes>
+    </BrowserRouter>
+  )
 }
 
 export default App
